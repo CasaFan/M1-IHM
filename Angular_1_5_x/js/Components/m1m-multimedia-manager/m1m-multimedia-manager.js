f90a31537e4ba136fa0daafcc9059edf644bfbfb
CasaFan <milk199357@gmail.com>
2017-01-23 18:40:57 +0100

    V2 Refactorying

--- Angular_1_5_x/js/Components/m1m-multimedia-manager/m1m-multimedia-manager.js
+++ Angular_1_5_x/js/Components/m1m-multimedia-manager/m1m-multimedia-manager.js
@@ -3,8 +3,6 @@ var angular = require("angular"),
     angularMaterial = require("angular-material"),
     ngDraggable = require("ng-draggable"),
     template = require("./m1m-multimedia-manager.html"),
-    //moment = require("./../../../bower_components/moment/min/moment.min.js"),
-    //m1mMediaRenders     = require("./../m1m-mediaRender-manager/m1m-mediaRender-manager.js"),
     m1mServers = require("./../m1m-server-manager/m1m-server-manager.js")
     ;
 module.exports = "m1m-multimedia-manager-Module";
@@ -15,8 +13,9 @@ console.log("Init of m1m-multimedia-manager-Module", CommModule, angularMaterial
 
 function controller($scope, CommService) {
     var ctrl = this;
-    
+
     $scope.playing = false;
+    $scope.volumeOK = true;
     $scope.mediaLength = 0;
     $scope.timerRunning = false;
 
@@ -26,6 +25,31 @@ function controller($scope, CommService) {
     this.currentRenderingControl;
     this.currentMediaMetaData;
     $scope.currentMediaData;
+    $scope.lastRender;
+
+    var defautRenderStyle = {
+        'height': '66px',
+        'text-align': 'center',
+        'border': '1px #eeeeee solid',
+        'margin-top': '10px'
+    }
+
+    var clickedRenderStyle = {
+        'height': '40px',
+        'text-align': 'center',
+        'border': '1px #eeeeee solid',
+        'margin-top': '10px',
+        'background-color': '#00a57d',
+        'position': 'fixed',
+        'width': '90%',
+        'opacity': '0.9',
+        'bottom': '10px',
+        'margin-bottom': '0',
+        'border-radius': '5px',
+        'color': 'white'
+    }
+
+    $scope.renderStyle = defautRenderStyle;
 
 
     console.log("m1mMultimediaManager:", $scope, CommService);
@@ -33,22 +57,24 @@ function controller($scope, CommService) {
 
     //TODO : try no render catch err
     $scope.currentRenderId = "";
-    
+
 
     CommService.onupdate = function () {
         $scope.$applyAsync(); // Mise à jour du rendu
     };
 
-    this.setRenderer = function (mediaRendererId) {
-        $scope.currentRenderId = mediaRendererId;
+    this.setRenderer = function (mediaRendererId, render) {
         this.getEtatLecteur(mediaRendererId);
+        if ($scope.lastRender != null) {
+            $scope.lastRender.renderStyle = defautRenderStyle;
+        }
+        $scope.currentRenderId = mediaRendererId;
+        render.renderStyle = clickedRenderStyle;
+        $scope.lastRender = render;
+
         console.log("set rendererId : ", mediaRendererId);
-        
+
     }
-    this.test = function () {
-        console.log("coucou");
-    }
-    
 
     this.play = function () {
         CommService.play($scope.currentRenderId);
@@ -80,24 +106,17 @@ function controller($scope, CommService) {
         $scope.$broadcast('timer-start');
         console.log("media loaded ");
     }
-    /**
-     * get etat courrant du lecteur 
-     * 
-     */
+
     this.getEtatLecteur = function (mediaRenderId) {
         utils.call(mediaRenderId, "getMediasStates", []).then(function (data) {
             console.log("dataGot =>", data);
             this.currentAVT = data['urn:schemas-upnp-org:service:AVTransport:1'];
-            
-            // TODO : get current media length
-            /*var momento = new moment(this.currentAVT['CurrentMediaDuration'], "HH:mm:ss");
-            $scope.mediaLength =  momento.hours()*3600 + momento.minutes()*60 + momento.seconds();
-            console.log("mediaLength : ", $scope.mediaLength);
-            */
+
             // getMediaName by parsing xml metaData to json
             var x2js = new X2JS();
             this.currentMediaMetaData = x2js.xml_str2json(this.currentAVT['CurrentTrackMetaData']);
             $scope.currentMediaData = this.currentMediaMetaData['DIDL-Lite'].item;
+
             console.log("media", this.currentMediaMetaData);
 
             // if status OK getState
@@ -122,6 +141,7 @@ function controller($scope, CommService) {
             //get volumeEtat & bind in scope
             this.currentRenderingControl = data['urn:schemas-upnp-org:service:RenderingControl:1'];
             $scope.volume = parseInt(this.currentRenderingControl['Volume'], 10);
+
         });
     }
 }
--- Angular_1_5_x/js/Components/m1m-server-manager/m1m-server-manager.js
+++ Angular_1_5_x/js/Components/m1m-server-manager/m1m-server-manager.js
@@ -8,7 +8,7 @@ module.exports = "m1m-server-manager";
 
 console.log("Init of m1m-server-manager-Module", CommModule, angularMaterial, ngDraggable);
 
-function controller($scope, CommService, $filter) {
+function controller($scope, CommService, $filter, $mdDialog) {
 
     var ctrl = this;
     console.log("m1mServerManager:", $scope, CommService);
@@ -16,6 +16,7 @@ function controller($scope, CommService, $filter) {
     $scope.serverName = "";
     $scope.currentServerID;
     $scope.listDirectories = [];
+    $scope.showHome = false;
 
     console.log("m1mServer:", this.mediaServers);
 
@@ -23,13 +24,14 @@ function controller($scope, CommService, $filter) {
         $scope.currentServerID = serveurID;
         this.browse(serveurID);
         $scope.listDirectories = [];
+        $scope.showHome = true;
     }
 
     CommService.onupdate = function () {
         $scope.$applyAsync(); // Mise à jour du rendu
     };
 
-    this.browse = function (mediaServerId, directoryId, directoryName) {
+    this.browse = function (mediaServerId, home, directoryId, directoryName) {
         CommService.browse(mediaServerId, directoryId).then(function (data) {
             console.log("Browse", mediaServerId, directoryId, "=>", data);
             ctrl.directories = data.directories;
@@ -37,51 +39,60 @@ function controller($scope, CommService, $filter) {
             $scope.$applyAsync();
         });
 
+        if (home) {
+            $scope.listDirectories = [];
+        }
+
         if (!angular.isUndefined(directoryId)) {
             $scope.listDirectories.push({ name: directoryName, dirID: directoryId });
-
             console.log("pushed list :", $scope.listDirectories);
+
             // path clic
             if (angular.isUndefined(directoryName)) {
                 //search index of dir in listDirectories
                 var item = $filter('filter')($scope.listDirectories, { dirID: directoryId })[0];
                 var index = $scope.listDirectories.indexOf(item);
-                $scope.listDirectories.splice(index+1);
-
+                $scope.listDirectories.splice(index + 1);
                 console.log("listDIr :", $scope.listDirectories, "index : ", index);
             }
+
         } else {
             // nav clic
             this.listDirectories = [];
-
         }
-    }
-    /*
-        this.browsePath = function(mediaServerId, directoryId){
-
-            // path clic
-            if (angular.isUndefined(directoryName)) {
-                //search index of dir in listDirectories
-                var item = $filter('filter')($scope.listDirectories, { dirID: directoryId })[0];
-                var index = $scope.listDirectories.indexOf(item);
-                $scope.listDirectories.splice(index);
-
-                console.log("listDIr :", $scope.listDirectories);
-            }
-
-            CommService.browse( mediaServerId, this.listParent[len]).then( function(data) {
-                console.log( "Browse", mediaServerId, this.listParent[this.listParent.length-1], "=>", data );
-                ctrl.directories = data.directories;
-                ctrl.medias = data.medias;
-                $scope.$applyAsync();
-            });
-          
-            console.log( "poped list : ", this.listParent );
-        }
-        */
         
+    }
+
+    this.openDescription = function (ev, media) {
+        console.log($scope);
+        $mdDialog.show({
+            targetEvent: ev,
+            template: '<md-dialog aria-label="media">'
+            + '<md-toolbar style="background-color: #19ad89;"><div class="md-toolbar-tools"><h2>'
+            + media.title + '</h2></div></md-toolbar>'
+            + '<md-dialog-content>'
+            + '<div class="md-dialog-content">'
+            + '<div style="text-aligne:center;"><img ng-src="' + media.icon + '" style="display: block;"/></div>'
+            + '<p><span style="color:#ff4081">Acteurs : </span>'
+            //+'<label ng-repeat="acteur in '+media.actors+'">{{acteur}}<'
+            + media.actors.toString() + '</p>'
+            + '<p><span style="color:#ff4081">Createurs : </span>' + media.creator + '</p>'
+            + '<p><span style="color:#ff4081">Date : </span>' + media.date + '</p>'
+            + '<p><span style="color:#ff4081">Description : </span>' + media.description + '</p>'
+            + '<p><span style="color:#ff4081">Genres : </span>' + media.genres.toString() + '</p>'
+            + '<p><span style="color:#ff4081">Long Description : </span>' + media.longdescription + '</p>'
+            + '</div>'
+            + '</md-dialog-content>'
+            + '</md-dialog>',
+            //templateUrl:'mediaDescription.tmpl.html',
+            clickOutsideToClose: true,
+            escapeToClose: true,
+            locals: { media: media }
+        });
+    };
+
 }
-controller.$inject = ["$scope", "CommService", "$filter"];
+controller.$inject = ["$scope", "CommService", "$filter", "$mdDialog"];
 
 angular.module(module.exports, [CommModule, angularMaterial, "ngDraggable"])
     .component("m1mServerManager", {
